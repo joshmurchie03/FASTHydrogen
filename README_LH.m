4979942d887852c23868f7dcffe93921fee3bc51
joshmurchie03 <127502840+joshmurchie03@users.noreply.github.com>
2025-10-28 18:28:09 +0000

    Delete README_LH.m

--- README_LH.m
+++
@@ -1,5 +0,0 @@
-% ========================================================================
-% THE CODE THAT HAS BEEN UPLOADED TO THIS GITHUB IS SOME FROM FAST THAT I
-% HAVE TWEAKED OR SLIGHTLY CHANGED TO SUIT MY HYDROGEN AIRCRAFT. THE REST
-% FILES IN FAST REMAIN UNCHANGED APART FROM THESE SPECIFIC FILES.
-% ========================================================================
